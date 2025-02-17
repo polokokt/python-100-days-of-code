27007ae416173861bf2d0bd7f1423be1519789ac
polokokt <stannnik@gmail.com>
2025-02-17 13:20:22 +0000

    feat: day-03 - if/else control flows and logical operators

---
+++ day-03/bmi_interpreter.py
@@ -0,0 +1,13 @@
+# BMI calculator with interpetation (if/else)
+
+weight = float(input("Enter your weight:\n "))
+height = float(input("Enter your height:\n "))
+
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print(f"Your weight is underweight with bmi = {bmi}")
+elif bmi >= 18.5 and bmi < 25:
+    print(f"Your weight is normal with bmi = {bmi}")
+else:
+    print(f"Your weight is overweight with bmi = {bmi}")
