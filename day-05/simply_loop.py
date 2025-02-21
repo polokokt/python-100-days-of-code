0a954812a1c8779fc51c254571e4e8962b6a0290
polokokt <stannnik@gmail.com>
2025-02-21 11:48:35 +0000

    feat: day-05 - loops and range function

---
+++ day-05/simply_loop.py
@@ -0,0 +1,6 @@
+fruits=["Apple","Banana","Lemon","Kiwi","Pear"]
+
+for fruit in fruits:
+    print(f"My favourite fruit is: {fruit}")
+    
+print(fruits)
---
+++ day-05/students_loop.py
@@ -0,0 +1,27 @@
+student_scores=[87, 125, 133, 98, 176, 59, 112, 162, 134, 78, 186, 83, 91, 177, 142, 150, 93, 61, 193, 171, 72, 118, 155, 80, 107, 137, 167, 139, 66, 120, 160, 145, 73, 88, 129]
+
+# function sum 
+print(f"Summary students scores: {sum(student_scores)}")
+
+#count sum in the loop
+
+total = 0
+
+for score in student_scores:
+    total += score
+
+print(f"Sum from the loop: {total}")
+
+# build in max function 
+
+print(f"Max value based on max function: {max(student_scores)}")
+
+# max value achieved from loop
+
+max = 0
+
+for number in student_scores:
+    if number > max:
+        max = number
+
+print(f"Max score from the loop: {max}")
