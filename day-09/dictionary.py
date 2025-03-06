735f953779a2c10bf8ae52b8a759d2af45ab9aaf
polokokt <stannnik@gmail.com>
2025-03-07 00:13:28 +0100

    feat: day-09 - Dictionaries, Nesting and the Secret Auction

---
+++ day-09/dictionary.py
@@ -0,0 +1,24 @@
+programming_dictionary = {
+    "Bug": "An error in a program..",
+    "Function": "A piece of code...",
+}
+
+print (programming_dictionary["Bug"])
+
+loop = input("Input the description for Loop: \n")
+
+programming_dictionary["Loop"] = loop
+
+print(programming_dictionary)
+
+#Wipe an existing dictionary
+
+#programming_dictionary = {}
+
+#print(programming_dictionary)
+
+# Loop for the dictionary
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
