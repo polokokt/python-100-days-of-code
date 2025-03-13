f249936691049147d210bde18bd92c88aa1ce015
polokokt <stannnik@gmail.com>
2025-03-13 23:11:41 +0100

    fix: day-01-project

--- day-01/day-1-project.py
+++ day-01/day-1-project.py
@@ -2,6 +2,6 @@
 # Input city name, pet's name and receive the best name for you band.  
 
 print("Welcome to the band name generator")
-city = input("What's the name of the city you gre up in?\n" + " ")
+city = input("What's the name of the city you grew up in? \n")
 pet = input("What's your pet's name?\n" + " ")
 print("Your band name could be " + city + " " + pet)
