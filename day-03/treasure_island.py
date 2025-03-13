4d24a5633406d1aadc28a61cb98629717d7c231e
polokokt <stannnik@gmail.com>
2025-03-14 00:16:54 +0100

    fix: black format v2

--- day-03/treasure_island.py
+++ day-03/treasure_island.py
@@ -2,12 +2,14 @@ print("Welcome to Treasury Island game!")
 print("Your mission is to find the treasure.")
 
 choice1 = input(
-    "You are on the cross road. Where you want to go?\n" 'Type "left" or "right"\n'
+    "You are on the cross road. Where you want to go?\n"
+    'Type "left" or "right"\n'
 ).lower()
 
 if choice1 == "left":
     choice2 = input(
-        "Do you want to swim over the lake or wait?\n" 'Type "wait" or "swim"\n'
+        "Do you want to swim over the lake or wait?\n"
+        'Type "wait" or "swim"\n'
     ).lower()
     if choice2 == "wait":
         doors = input(
--- day-04/who_pay.py
+++ day-04/who_pay.py
@@ -7,5 +7,7 @@ print(f"Today {random.choice(friends)} pays according to option 1")
 
 # Option 2
 print(
-    f"Todat {friends[random.randint(0, len(friends)-1)]} " f"pays accordin to option 2"
+    f"Todat {friends[random.randint(0, len(friends)-1)]} "
+    f"pays accordin to option 2"
 )
+
