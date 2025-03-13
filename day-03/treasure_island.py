51582eceebd9c6b744897cbe3140d1e768443b6d
polokokt <stannnik@gmail.com>
2025-03-14 00:26:31 +0100

    fix: black issue

--- day-03/treasure_island.py
+++ day-03/treasure_island.py
@@ -1,21 +1,27 @@
 print("Welcome to Treasury Island game!")
 print("Your mission is to find the treasure.")
 
-choice1 = input((
-    "You are on the cross road. Where you want to go?\n"
-    'Type "left" or "right"\n'
-)).lower()
+choice1 = input(
+    (
+        "You are on the cross road. Where you want to go?\n"
+        'Type "left" or "right"\n'
+    )
+).lower()
 
 if choice1 == "left":
-    choice2 = input((
-        "Do you want to swim over the lake or wait?\n"
-        'Type "wait" or "swim"\n'
-    )).lower()
+    choice2 = input(
+        (
+            "Do you want to swim over the lake or wait?\n"
+            'Type "wait" or "swim"\n'
+        )
+    ).lower()
     if choice2 == "wait":
-        doors = input((
-            "You found a house with three doors. Which doors you choice?\n"
-            'Type "blue", "red" or "yellow"\n'
-        )).lower()
+        doors = input(
+            (
+                "You found a house with three doors. Which doors you choice?\n"
+                'Type "blue", "red" or "yellow"\n'
+            )
+        ).lower()
         if doors == "yellow":
             print("You win! You found the treasure !")
         elif doors == "red" or doors == "blue":
