a9c760d7a012451065d48179d0e9e4e18436110b
polokokt <stannnik@gmail.com>
2025-03-14 00:22:26 +0100

    fix: remove blank line v2

--- day-03/treasure_island.py
+++ day-03/treasure_island.py
@@ -1,21 +1,21 @@
 print("Welcome to Treasury Island game!")
 print("Your mission is to find the treasure.")
 
-choice1 = input(
+choice1 = input((
     "You are on the cross road. Where you want to go?\n"
     'Type "left" or "right"\n'
-).lower()
+)).lower()
 
 if choice1 == "left":
-    choice2 = input(
+    choice2 = input((
         "Do you want to swim over the lake or wait?\n"
         'Type "wait" or "swim"\n'
-    ).lower()
+    )).lower()
     if choice2 == "wait":
-        doors = input(
+        doors = input((
             "You found a house with three doors. Which doors you choice?\n"
             'Type "blue", "red" or "yellow"\n'
-        ).lower()
+        )).lower()
         if doors == "yellow":
             print("You win! You found the treasure !")
         elif doors == "red" or doors == "blue":
--- day-04/who_pay.py
+++ day-04/who_pay.py
@@ -6,7 +6,7 @@ friends = ["Adam", "Maciej", "Anna", "Damian", "Weronika"]
 print(f"Today {random.choice(friends)} pays according to option 1")
 
 # Option 2
-print(
+print((
     f"Todat {friends[random.randint(0, len(friends)-1)]} "
     f"pays accordin to option 2"
-)
+))
