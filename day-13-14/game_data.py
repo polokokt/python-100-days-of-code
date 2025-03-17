a5f8d5cb8ff919daf97215c5650562411796be7b
polokokt <stannnik@gmail.com>
2025-03-17 23:28:37 +0100

    feat: day-13-14: exceptions and Higher or Lower game

---
+++ day-13-14/game_data.py
@@ -0,0 +1,27 @@
+data = [
+    {
+        "name": "Mistrzostwa Europy w Piłce Nożnej 2024",
+        "follower_count": 500,
+        "description": "Turniej piłkarski",
+        "country": "Europa",
+    },
+    {
+        "name": "Wybory w USA",
+        "follower_count": 450,
+        "description": "Wybory prezydenckie w Stanach Zjednoczonych",
+        "country": "Stany Zjednoczone",
+    },
+    {"name": "Budda", "follower_count": 400, "description": "Pseudonim popularnego YouTubera", "country": "Polska"},
+    {"name": "TV Republika", "follower_count": 350, "description": "Polska stacja telewizyjna", "country": "Polska"},
+    {"name": "Wybory samorządowe 2024", "follower_count": 300, "description": "Lokalne wybory w Polsce", "country": "Polska"},
+    {
+        "name": "Wyniki wyborów do Parlamentu Europejskiego",
+        "follower_count": 280,
+        "description": "Wyniki wyborów do PE",
+        "country": "Europa",
+    },
+    {"name": "Igrzyska Olimpijskie", "follower_count": 260, "description": "Międzynarodowe zawody sportowe", "country": "Świat"},
+    {"name": "Jerzy Stuhr", "follower_count": 240, "description": "Polski aktor i reżyser", "country": "Polska"},
+    {"name": "Liam Payne", "follower_count": 220, "description": "Brytyjski piosenkarz", "country": "Wielka Brytania"},
+    {"name": "Julia Szeremeta", "follower_count": 200, "description": "Postać medialna", "country": "Polska"},
+]
